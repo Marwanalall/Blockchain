1b6a4ff39085feb02293f8689fedd2d2374f38c2
Marwanalall <malaloush2016@yahoo.com>
2022-10-12 10:13:52 +0000

    tex

---
+++ block.js
@@ -0,0 +1,23 @@
+class Block{
+
+
+
+    constructor(timestamp,lastHash,hash,data){
+
+        this.timestamp = timestamp; // Zeitstempel
+
+        this.lastHash = lastHash; // Hash des vorhergehenden Blocks
+
+        this.hash = hash; // Eigener Hash
+
+        this.data = data; // Daten
+
+    }
+
+
+
+
+
+
+
+}
